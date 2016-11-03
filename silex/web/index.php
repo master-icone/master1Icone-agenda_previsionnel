5b64b0d0894ec2723cd5947d8a18c898d8fc3a9d
Guillaume HUGUES <guillaume.hugues@hotmail.fr>
2016-11-03 14:38:54 +0100

    Parti mdp crypté ajoutée

--- silex/web/index.php
+++ silex/web/index.php
@@ -4,24 +4,41 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-#lien nécessaire http://localhost/previsionnel/silex/web/ ou /silex/web/index.php
-$app->get('/', function () {
-    return 'Something !';
-});
-
-#lien nécessaire http://localhost/previsionnel/silex/web/index.php/hello
-$app->get('/hello', function () {
-    return 'Hello!';
-});
-
-#exemple lien nécessaire http://localhost/previsionnel/silex/web/index.php/hello/Simon
-$app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello ' . $app->escape($name);
-});
-
-#exemple lien nécessaire http://localhost/previsionnel/silex/web/index.php/ajouter/name=Guillaume&surname=Hugues&statut=2
-$app->get('/ajouter/name={name}&surname={surname}&statut={idStatut}', function ($name,$surname,$idStatut) use ($app) {
-    return $app->escape($name) . ' ' . $app->escape($surname) . ' avec l\'id statut ' . $app->escape($idStatut) . ' a bien été ajouté(e)';
+$app->get('/test', function () {
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=projet_m1;charset=utf8', 'root', '');
+	}
+	catch (Exception $e)
+	{
+			die('Erreur : ' . $e->getMessage());
+	}
+	
+	$log = 'Tran';
+	$pass = 'ESA35IAV4GR';
+	$hash = password_hash($pass,PASSWORD_BCRYPT,['cost' => 13]);
+	$sql = $bdd->query("UPDATE personnel SET password='".$hash."' WHERE login='".$log."'");
+	
+	$req = $bdd->query("SELECT * FROM personnel WHERE login='".$log."'");
+	while ($donnees = $req->fetch())
+	{
+	?>
+		<p>
+		<strong>Nom</strong> : <?php echo $donnees['nom']; ?><br />
+		<strong>Prénom</strong> : <?php echo $donnees['prenom']; ?><br />
+	   </p>
+	<?php
+	}
+	
+	$hashPassDef = $bdd->query("SELECT password FROM personnel WHERE login='".$log."'");
+	$donnees = $hashPassDef->fetch();
+	if(password_verify($pass, $donnees['password'])) {
+		echo 'OK 1er';
+	} else {
+		echo 'ERREUR 1er';
+	}
+	$req->closeCursor();
+	return '';
 });
 
 $app->run();
