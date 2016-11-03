7476a286958c26c2aa19715495234380b69ceea9
Simon Fillaudeau <simonfillaudeau@gmail.com>
2016-11-03 10:56:03 +0100

    Post example 2
    This is a second example but this one is related to the database

--- silex/web/index.php
+++ silex/web/index.php
@@ -30,11 +30,20 @@ $app->get('/ajouter/name={name}&surname={surname}&statut={idStatut}', function (
     return $app->escape($name) . ' ' . $app->escape($surname) . ' avec l\'id statut ' . $app->escape($idStatut) . ' a bien été ajouté(e)';
 });
 
+#il faut faire un appel à ça comme dans test.html situé dans le dossier précédent
 $app->post('/feedback', function (Request $request) {
     $name = $request->get('name');
     $pw = $request->get('pw');
 	
 	return 'Test pour ' . $name . ' dont le mot de passe est ' . $pw;
+});
+
+#il faut faire un appel à ça comme dans test.html situé dans le dossier précédent
+$app->post('/addPersonnel', function (Request $request) {
+	// $request->get("login") permet d'accéder au champ avec la valeur login de ce qui est envoyé en post
+	echo 'Insert into Personnel values (\'' . $request->get('login') . '\', \'' . $request->get('pw') . '\', \'' . $request->get('name') . '\', \'' . $request->get('firstname') . '\')';
+	echo "<br/><br>";
+	return 'Insertion réussi';
 });
 
 $app->run();
