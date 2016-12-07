78d96a4895a3d7b4b8ea4f90e0402a7f1914dc3b
Mr Agbani <Batro-92@eduroam05029.univ-lr.fr>
2016-12-07 09:52:27 +0000

    changement vueglobale to accueil

---
+++ silex/front_office/app/components/accueil/accueil.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'accueil',
+  templateUrl: '../../../app/components/accueil/accueil.html',
+  styleUrls: ['../../../app/components/accueil/accueil.css']
+
+})
+
+export class AccueilComponent { }    // € Exportation du component sous la forme du nom de class
--- silex/front_office/app/components/vueGlobale/vueGlobale.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'vueGlobale',
-  templateUrl: '../../../app/components/vueGlobale/vueGlobale.html',
-  styleUrls: ['../../../app/components/vueGlobale/vueGlobale.css']
-
-})
-
-export class VueGlobaleComponent { }    // € Exportation du component sous la forme du nom de class
