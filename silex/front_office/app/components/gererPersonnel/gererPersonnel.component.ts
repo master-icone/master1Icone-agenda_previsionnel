1e4775e80857c7431f22747eb081d0e212e250db
dsagnier <sagnier.dimitri@gmail.com>
2016-12-11 15:54:54 +0100

    v1 de la gestion de personnel

--- silex/front_office/app/components/gererPersonnel/gererPersonnel.component.ts
+++ silex/front_office/app/components/gererPersonnel/gererPersonnel.component.ts
@@ -1,7 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../services/http.service';
+
 @Component({
   selector: 'gererPersonnel',
+  providers: [HttpService],
   templateUrl: '../../../app/components/gererPersonnel/gererPersonnel.html'
 })
 
-export class GererPersonnelComponent { }
+export class GererPersonnelComponent {
+  link = 'http://localhost:3000/gererPersonnel';
+  listePersonnel: any;
+  personnel: any;
+
+  constructor (private _httpService: HttpService) { }
+
+  ngOnInit() {
+    this.getListePersonnel();
+  }
+
+  getListePersonnel() {
+    this._httpService.httpGet(this.link)
+        .subscribe(
+          data => {
+            this.listePersonnel = data;
+          },
+          error => alert(error),
+          () => console.log("Finished")
+        );
+  }
+  getPersonnel(id) {
+    this._httpService.httpGet(this.link+"/"+id)
+        .subscribe(
+          data => {
+            this.personnel = data;
+          },
+          error => alert(error),
+          () => console.log("Finished")
+        );
+  }
+}
