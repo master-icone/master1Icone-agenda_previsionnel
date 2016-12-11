e2b1a076029a47576697dc4dce0450cd7ee27e90
fouine17 <fouineetmaurice@gmail.com>
2016-12-11 04:38:46 +0100

    Mise à jour du JSON db.json

--- silex/front_office/app/components/decharges/decharges.component.ts
+++ silex/front_office/app/components/decharges/decharges.component.ts
@@ -1,7 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../services/http.service';
+
 @Component({
   selector: 'decharges',
-  templateUrl: '../../../app/components/decharges/decharges.html'
+  templateUrl: '../../../app/components/decharges/decharges.html',
+  providers: [HttpService]
+  
 })
 
-export class DechargesComponent { }
+export class DechargesComponent implements OnInit{
+  link = 'http://localhost:3000/decharges';
+  decharges: any;
+
+  
+  constructor (private _httpService: HttpService) { }
+
+  ngOnInit() {
+    this.getDecharges();
+  }
+
+  getDecharges() {
+    this._httpService.httpGet(this.link)
+        .subscribe(
+          data => {
+            this.decharges = data;
+          },
+          error => alert(error),
+          () => console.log("Finished")
+        );
+  }
+}
