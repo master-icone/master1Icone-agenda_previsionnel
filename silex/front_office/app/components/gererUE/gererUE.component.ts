d9083c369e8275b4890d13bddea060d92ccdf605
fouine17 <fouineetmaurice@gmail.com>
2016-12-11 23:26:50 +0100

    adaptation du dossier gererUE avec liens cliquables

--- silex/front_office/app/components/gererUE/gererUE.component.ts
+++ silex/front_office/app/components/gererUE/gererUE.component.ts
@@ -1,7 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../services/http.service';
+
 @Component({
   selector: 'gererUE',
+  providers: [HttpService],
   templateUrl: '../../../app/components/gererUE/gererUE.html'
 })
 
-export class GererUEComponent { }
+export class GererUEComponent implements OnInit{
+  link = 'http://localhost:3000/gererUE';
+  listeUE: any;
+  UE: any;
+
+  constructor (private _httpService: HttpService) { }
+
+  ngOnInit() {
+    this.getListeUE();
+  }
+
+  getListeUE() {
+    this._httpService.httpGet(this.link)
+        .subscribe(
+          data => {
+            this.listeUE = data;
+          },
+          error => alert(error),
+          () => console.log("Finished")
+        );
+  }
+  getUE(id) {
+    this._httpService.httpGet(this.link+"/"+id)
+        .subscribe(
+          data => {
+            this.UE = data;
+          },
+          error => alert(error),
+          () => console.log("Finished")
+        );
+  }
+}
