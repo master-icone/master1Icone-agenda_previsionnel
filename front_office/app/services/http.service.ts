8f13253d2a86405e79afc19688eac22f9d7d0ba4
dsagnier <sagnier.dimitri@gmail.com>
2016-12-12 14:57:50 +0100

    Création de la méthode httpPost

--- front_office/app/services/http.service.ts
+++ front_office/app/services/http.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -11,7 +11,16 @@ export class HttpService {
     return this._http.get(link)
         .map(res => res.json());
   }
-  httpPost() {
-    
+
+  httpPost(json: string, link: string) {
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    this._http.post(link, json, {
+      headers: headers
+    }).subscribe(data => {
+                alert('ok');
+          }, error => {
+              console.log(JSON.stringify(error.json()));
+          });
   }
 }
