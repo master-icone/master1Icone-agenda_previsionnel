e5e35ab4ad368d00e47749339d943197d2e88c07
dsagnier <sagnier.dimitri@gmail.com>
2016-12-13 01:58:18 +0100

    Mise en place de children Route et la création des components de gererStatuts

---
+++ front_office/app/components/gererStatuts/selectStatuts.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { HttpService } from '../../services/http.service';
+
+@Component({
+  selector: 'selectStatuts',
+  templateUrl: '../../../app/components/gererStatuts/selectStatuts.html'
+})
+
+export class SelectStatutsComponent { }
---
+++ front_office/app/components/gererStatuts/vueStatuts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { HttpService } from '../../services/http.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'vueStatuts',
+  templateUrl: '../../../app/components/gererStatuts/vueStatuts.html'
+})
+
+export class VueStatutsComponent { }
