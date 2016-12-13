af609f8a5f998918038cd32071d5cb4d2e1c347c
dsagnier <sagnier.dimitri@gmail.com>
2016-12-13 16:25:53 +0100

    Fonctionalité d'ajout d'un statut

--- front_office/app/components/gererStatuts/ajouterStatuts.component.ts
+++ front_office/app/components/gererStatuts/ajouterStatuts.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { HttpService } from '../../services/http.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'ajouterStatuts',
-  templateUrl: '../../../app/components/gererStatuts/ajouterStatuts.html'
+  templateUrl: '../../../app/components/gererStatuts/ajouterStatuts.html',
+  providers: [HttpService]
 })
 
-export class AjouterStatutsComponent { }
+export class AjouterStatutsComponent {
+  link = 'http://localhost:3000/gererStatuts';
+  label: string;
+  heures: string;
+  autorisation: string;
+  data: string;
+
+  constructor (private _httpService: HttpService, private router: Router) { }
+
+  postData() {
+    this.data = 'label='+this.label+'&heures='+this.heures+'&autorisation='+this.autorisation;
+    this._httpService.httpPost(this.link, this.data)
+        .subscribe(
+          data => { },
+          error => alert(error),
+          () => console.log("Finished")
+        );
+    this.router.navigate(['./gererStatuts']);
+  }
+}
