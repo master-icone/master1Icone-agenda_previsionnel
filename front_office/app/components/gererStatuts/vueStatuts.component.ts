f311ca88b9c0dcab3c14f19a70d82cd4e01aaef5
dsagnier <sagnier.dimitri@gmail.com>
2016-12-15 00:40:06 +0100

    Fonctionnalité pour ajouter un status à un prof

--- front_office/app/components/gererStatuts/vueStatuts.component.ts
+++ front_office/app/components/gererStatuts/vueStatuts.component.ts
@@ -14,8 +14,16 @@ export class VueStatutsComponent {
   id: any;
   urlStatuts = 'http://localhost:3000/gererStatuts';
   statuts: any;
+  // Get personnel by statut
   urlPersonnels = 'http://localhost:3000/listePersonnelByStatut?statut=';
   personnels: any;
+  // Add personnel to statut
+  urlPersonnelsAdd = 'http://localhost:3000/listePersonnelByStatut';
+  urlPersonnelsOpt = 'http://localhost:3000/listePersonnelByStatut?nom=';
+  personnelsAdd: any;
+  personnelAdd: any;
+  personnelAd: any;
+  resultAdd: any;
 
   constructor (private _httpService: HttpService,
                params: ActivatedRoute,
@@ -39,34 +47,11 @@ export class VueStatutsComponent {
     if(this.id) {
       this.getStatut(this.id);
       this.getPersonnel(this.id);
+      this.getPersonnelAdd();
     }
   }
 
-  confirm() {
-    this.confirmationService.confirm({
-            message: 'Êtes vous sur de vouloir supprimer ce statut ?',
-            header: 'Confirmer la suppression',
-            icon: ' 	glyphicon glyphicon-info-sign',
-            accept: () => {
-              this.communicateService.setDisplay();
-              this.deleteStatut(this.id);
-            }
-        });
-  }
-
-  getPersonnel(id) {
-    this._httpService.httpGet(this.urlPersonnels+id)
-        .subscribe(
-          data => {
-            this.personnels = data;
-          },
-          error => {alert(error);
-            this.router.navigate(['./accueil']);
-          },
-          () => console.log("Finished")
-        );
-  }
-
+  // Afficher la fiche statut
   getStatut(id) {
     this._httpService.httpGet(this.urlStatuts+"/"+id)
         .subscribe(
@@ -80,6 +65,21 @@ export class VueStatutsComponent {
         );
   }
 
+  // Afficher la liste des personnel du statut
+  getPersonnel(id) {
+    this._httpService.httpGet(this.urlPersonnels+id)
+        .subscribe(
+          data => {
+            this.personnels = data;
+          },
+          error => {alert(error);
+            this.router.navigate(['./accueil']);
+          },
+          () => console.log("Finished")
+        );
+  }
+
+  // Suppression d'un statut
   deleteStatut(id) {
     this._httpService.httpDelete(this.urlStatuts+"/"+id)
         .subscribe(
@@ -90,6 +90,62 @@ export class VueStatutsComponent {
           error => {
             this.router.navigate(['./accueil']);
           },
+          () => console.log("Finished")
+        );
+  }
+
+  // Confirmer la suppression
+  confirm() {
+    this.confirmationService.confirm({
+            message: 'Êtes vous sur de vouloir supprimer ce statut ?',
+            header: 'Confirmer la suppression',
+            icon: ' 	glyphicon glyphicon-info-sign',
+            accept: () => {
+              this.communicateService.setDisplay();
+              this.deleteStatut(this.id);
+            }
+        });
+  }
+
+  // Ajouter un personnel à un statut
+  getPersonnelAdd() {
+    this._httpService.httpGet(this.urlPersonnelsAdd)
+        .subscribe(
+          data => {
+            this.personnelsAdd = data;
+          },
+          error => {alert(error);
+            this.router.navigate(['./accueil']);
+          },
+          () => console.log("Finished")
+        );
+  }
+
+
+  updateStatutPersonnel() {
+    this._httpService.httpGet(this.urlPersonnelsOpt+this.personnelAdd)
+        .subscribe(
+          data => {
+            this.personnelAd = data[0];
+            this.resultAdd = '{"nom": "'+this.personnelAd.nom+'","prenom":"'+this.personnelAd.prenom+'","statut":'+this.id+'}';
+            this.putPersonnelInStatut();
+          },
+          error => {alert(error);
+            this.router.navigate(['./accueil']);
+          },
+          () => console.log("Finished")
+        );
+
+
+  }
+
+  putPersonnelInStatut() {
+    this._httpService.httpPut(this.urlPersonnelsAdd+'/'+this.personnelAd.id, this.resultAdd)
+        .subscribe(
+          data => {
+            this.display();
+          },
+          error => alert(error),
           () => console.log("Finished")
         );
   }
